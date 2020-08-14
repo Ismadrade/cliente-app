201a9c3abfdf4841156697e39a559714cd7d6b86
Ismael <maelandrade@yahoo.com.br>
2020-08-14 20:51:38 -0300

    Criando validacao

---
+++ src/app/servico-prestado.service.ts
@@ -0,0 +1,29 @@
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
+import { Observable } from 'rxjs';
+import { ServicoPrestado } from './servico-prestado/ServicoPrestado';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicoPrestadoService {
+
+  apiURL: string = environment.apiURLBase + "/api/servicos-prestados";
+
+  constructor( private http: HttpClient) { }
+
+  salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado>{
+    return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado);
+  }
+
+  buscar(nome: string, mes: number ): Observable<ServicoPrestadoBusca[]>{
+    const httpParams = new HttpParams().set("nome", nome).set("mes", mes ? mes.toString() : '');
+   
+    const url = this.apiURL + "?" + httpParams.toString();
+    console.log(url);
+  
+    return this.http.get<any>(url);
+  }
+}
